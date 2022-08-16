28dffd144ab1bed2eb3aa68e6bc17f18fbffc9bd
Taren <cyril.fahlenbock@uni-duesseldorf.de>
2022-08-16 18:19:08 +0200

    Try diffing without considering merged classes

--- src/Overeasy/Diff.hs
+++ src/Overeasy/Diff.hs
@@ -91,7 +91,7 @@ instance (Lattice d) => Lattice (EDiff d) where
 instance  (Diff d i, Lattice i, Eq i) => Diff (EGraph d f) (EDiff i) where
     diff base new 
       -- | not $ ILS.null missingDirty = error ("missingDirty: " ++ show missingDirty ++ " maps: " ++ show smarty ++ " merged: " <> show deadClasses <> "\n\nold epoch" <>  show (egEpoch base) <> " new epoch " <> show (egEpoch new) <> " new timestamps" <> show (egAnaTimestamps new))
-      | otherwise = EDiff (max (egEpoch base) (egEpoch new)) merged (MapDiff $ ILM.union diffAnalysis1 diffAnalysis2 )
+      | otherwise = EDiff (max (egEpoch base) (egEpoch new)) merged (MapDiff $ diffAnalysis1 )
       where
         merged = diff (egEquivFind base) (egEquivFind new)
         -- missingDirty= ILS.difference (ILS.fromList (ILM.keys diffAnalysis)) smarty
